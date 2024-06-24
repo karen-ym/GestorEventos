74da60a11d8ce88ab1ef5dd0edd4fc2224258bcf
JorgePomaV <jpomavergara1418@gmail.com>
2024-06-24 20:22:17 -0300

    Se agrego cambios a WedAdmin

---
+++ GestorEventos.WebAdmin/Controllers/EventosController.cs
@@ -0,0 +1,126 @@
+﻿using GestorEventos.Servicios.Servicios;
+using GestorEventos.Servicios.Entidades;
+using GestorEventos.Servicios.Servicios;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestorEventos.WebAdmin.Controllers
+{
+    public class EventosController : Controller
+    {
+        private IEventoService eventoService;
+        private IPersonaService personaService;
+
+        public EventosController(IEventoService _eventoService, IPersonaService _personaService)
+        {
+            this.eventoService = _eventoService;
+            this.personaService = _personaService;
+        }
+
+        // GET: EventosController
+        public ActionResult Index()
+        {
+            var eventos = this.eventoService.GetAllEventosViewModel();
+
+            return View(eventos);
+        }
+
+        // GET: EventosController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: EventosController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EventosController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+
+            try
+            {
+       
+                Persona personaAgasajada = new Persona();
+                personaAgasajada.Nombre = collection["Nombre"].ToString();
+                personaAgasajada.Apellido = collection["Apellido"].ToString();
+                personaAgasajada.Email = collection["Email"].ToString();
+                personaAgasajada.Telefono = collection["Telefono"].ToString();
+                personaAgasajada.Borrado = false;
+                personaAgasajada.Direccion = collection["Direccion"].ToString();
+
+                int IdPersonaAgasajada = personaService.AgregarNuevaPersona(personaAgasajada);
+
+
+
+
+                Evento eventoNuevo = new Evento();
+                eventoNuevo.IdPersonaAgasajada = IdPersonaAgasajada;
+
+                eventoNuevo.CantidadPersonas = int.Parse(collection["CantidadPersonas"].ToString());
+                eventoNuevo.Visible = true;
+                eventoNuevo.IdUsuario = int.Parse(HttpContext.User.Claims.First(x => x.Type == "usuarioSolterout").Value); // HttpContext.User.Identity.Id;
+                eventoNuevo.FechaEvento = DateTime.Parse(collection["FechaEvento"].ToString());
+                eventoNuevo.IdTipoEvento = int.Parse(collection["IdTipoEvento"].ToString());
+                eventoNuevo.NombreEvento = collection["NombreEvento"].ToString();
+                eventoNuevo.IdEstadoEvento = 2; //Pendiente de Aprobacion
+
+
+                this.eventoService.PostNuevoEvento(eventoNuevo);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EventosController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EventosController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EventosController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EventosController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
