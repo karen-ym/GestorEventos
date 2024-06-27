573921fa328f2b210227f16f3756ed3bd81a5d6a
Karen <karenymorel@gmail.com>
2024-06-27 16:21:02 -0300

    Corrección de bug

--- GestorEventos.Servicio/Entidades/Usuario.cs
+++ GestorEventos.Servicio/Entidades/Usuario.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GestorEventos.Servicios.Entidades
 {
-    internal class Usuario
+    public class Usuario
     {   
         public int IdUsuario { get; set; }
         public string GoogleIdentificador { get; set; }
