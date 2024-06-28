99317159ea7ba660a95ada6214d05561446aac3a
JorgePomaV <jpomavergara1418@gmail.com>
2024-06-28 14:17:14 -0300

    Se agrego el html de WebUsuario

--- GestorEventos.WebUsuario/wwwroot/js/site.js
+++ GestorEventos.WebUsuario/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+/*!
+* Start Bootstrap - One Page Wonder v6.0.6 (https://startbootstrap.com/theme/one-page-wonder)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-one-page-wonder/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
